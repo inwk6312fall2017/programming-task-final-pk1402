f5fe2c6b4cddd00e48a054feb5a83e30e25177f8
pk1402 <you@example.com>
2017-10-20 12:10:48 -0700

    calling self

--- wft3.py
+++ wft3.py
@@ -27,10 +27,18 @@ def compute_stats(self):
 
 	day_count = 0
 	fc = location.forcasts()[:5]
-for forecasts in forecastslist:
+for forecasts in fc:
 		self.dates.append('date')
 		self.high_temperature.append(forecasts['high'])
 		self.low_temperature.append(forecasts['low'])
 		if forcast_text.find('Rain') != -1:
 			self.rainy_days_index.appedn(day_count)
 		day_count += 1
+def print_stats(self):
+	print('current_condition: ',self.current_condition)
+	print('highest temperature : ',self.dates[self.find_min_index])
+	print('lowest temperature : ',self.dates[self.find_max_index])
+	if len(self.rainy_days_index) > 0:
+		print("IT will be raining ")
+		for i in self.rainy_days_index:
+			print(self.dates[i])
