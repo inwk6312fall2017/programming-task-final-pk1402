7858f793b394f46aad1d23aeb99b02dab4c3ff46
pk1402 <you@example.com>
2017-10-20 12:23:56 -0700

    update the ip address

--- cft3.py
+++ cft3.py
@@ -3,3 +3,10 @@ with open("running-config.cfg",'r') as current:
 	change_line=current_read()
 current.closed()
 
+rel_f=re.sub("ip address 172./",change_line)
+
+with open("running-config.cfg",'r') as update:
+        change_line=update_read()
+update.closed
+
+
