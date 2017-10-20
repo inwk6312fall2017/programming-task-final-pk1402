fa7cf5502eab1205cf3d6234409ef03afdd2be19
pk1402 <you@example.com>
2017-10-20 10:25:23 -0700

    before longest line now its prints longest word

--- ft.py
+++ ft.py
@@ -1,11 +1,23 @@
-f1=open("Book1.txt","r")
-read_file=f1.read()
-words=read_file.split()
-longest = 0
-for word in read_file.split():
-    if len(word) > longest:
-        longest = len(word)
-        longest_word = word
+#f1=open("Book1.txt","r")
+#read_file=f1.read()
+#words=read_file.split()
+#longest = 0
+#for word in read_file.split():
+    #if len(word) > longest:
+        #longest = len(word)
+        #longest_word = word
  
-print( "The longest word is %s" % longest_word )
+#print( "The longest word is %s" % longest_word )
+f1 = open('Book1.txt','r')
+read_file= f1.readlines()
+string = read_file[0]
+stringsplit = string.split()
+d = []
+for i in stringsplit:
+    d.append(len(i))
+    e = max(d)
+for j in stringsplit:
+    if len(j) == e:
+        print("The longest word is", j)
+
 
