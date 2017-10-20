a7245bd369956f345b836a821a88cd5b45017fd7
pk1402 <you@example.com>
2017-10-20 12:03:45 -0700

    declaration error

--- wft3.py
+++ wft3.py
@@ -1,7 +1,7 @@
 #sucessfully installed weather api in the  pc
 from weather import Weather
 weather = Weather
-dummy=Weather
+#dummy=Weather
 # Get weather forecasts for the upcoming days.
 
 #for forecasts in location.forecast():
@@ -9,9 +9,21 @@ dummy=Weather
     #print forecasts['date']
     #print forecasts['high']
     #print forecasts['low']
-forecastslist = location.forcast()[:5]
-for forcast in forecastslist:
+fc = location.forcast()[:5]
+for forcast in fc:
 	print(forcasts['text'])
 	print(forcasts['date'])
 	print(forcasts['high'])
 	print(forcasts['low'])
+def compute_stats(self)
+	weather_handle = weather.Weather()
+	self.high_temperature= []
+	self.low_temperature=[]
+	self.rainy_days_index=[]
+	self.current_condition= ''
+	location = self.weather_handle.lookup_by _location(self.metropolis)
+	condition = location.condition()
+	self.current_condition = condition['text']
+
+	day_count = 0
+	fc = location.forcasts()[:5]
